7cb2327b37c3336cab00abdf032b8376ac8d213b
Tejas Bubane <tejasbubane@gmail.com>
2025-01-06 00:17:48 +0400

    Add validation for sold tickets count

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,6 +8,11 @@ class Event < ApplicationRecord
   validates :starts_at, presence: true
   validates :ends_at, presence: true
   validates :total_tickets, presence: true
+  validate do |event|
+    if event.sold_tickets.to_i > event.total_tickets.to_i
+      errors.add(:sold_tickets, message: "must be equal to or less than total tickets")
+    end
+  end
 
   def available_tickets
     total_tickets.to_i - sold_tickets.to_i
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Event, type: :model do
     it { is_expected.to validate_presence_of(:starts_at) }
     it { is_expected.to validate_presence_of(:ends_at) }
     it { is_expected.to validate_presence_of(:total_tickets) }
+
+    describe "sold_tickets" do
+      it "fails when sold tickets are more than total tickets" do
+        event = build(:event, total_tickets: 20, sold_tickets: 21)
+        expect(event).not_to be_valid
+        expect(event.errors[:sold_tickets]).to eq([ "must be equal to or less than total tickets" ])
+      end
+    end
   end
 end
