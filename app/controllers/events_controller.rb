5f1547eed78460ccc8c46eda4963e55601a4ad7b
Tejas Bubane <tejasbubane@gmail.com>
2025-01-05 19:11:44 +0400

    Create event form flow

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,25 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: %i[name description starts_at ends_at total_tickets])
   end
 end
