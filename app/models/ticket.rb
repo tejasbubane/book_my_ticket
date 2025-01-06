41fe928378aebfc0d96a5e654761806b70c1a489
Tejas Bubane <tejasbubane@gmail.com>
2025-01-06 19:36:51 +0400

    Show number of booked tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,7 @@ class Ticket < ApplicationRecord
   # Associations
   belongs_to :event
   belongs_to :user
+
+  # Scopes
+  scope :for_user, ->(user) { where(user: user) }
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Ticket, type: :model do
     it { is_expected.to belong_to(:event) }
     it { is_expected.to belong_to(:user) }
   end
+
+  describe "scopes" do
+    let(:user) { create(:user) }
+    let!(:user_tickets) { create_list(:ticket, 2, user:) }
+    let!(:other_tickets) { create_list(:ticket, 2) }
+
+    it "returns tickets for given user" do
+      expect(Ticket.for_user(user).ids).to match_array(user_tickets.map(&:id))
+    end
+  end
 end
