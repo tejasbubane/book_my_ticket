11cf116d1437b93dbced1b90035ab5ae061b860d
Tejas Bubane <tejasbubane@gmail.com>
2025-01-08 11:20:57 +0400

    Make create event spec dynamic

--- spec/features/create_events_spec.rb
+++ spec/features/create_events_spec.rb
@@ -5,6 +5,7 @@ feature "User creates event" do
   let(:password) { "super_secret_password" }
   let(:event_name) { "First Event" }
   let!(:current_user) { create(:user, email:, password:, password_confirmation: password) }
+  let(:event_date) { Date.tomorrow }
 
   scenario "when all data is correct" do
     log_in_with email, password
@@ -13,7 +14,7 @@ feature "User creates event" do
     fill_in "Name", with: event_name
     fill_in "Description", with: "This is my first event"
     fill_in "Location", with: "Dubai Mall"
-    fill_in "Starts at", with: "07 / 01 / 2025, 01:00 pm"
+    fill_in "Starts at", with: event_date.strftime("%d / %m / %Y, %I:%M %P")
     fill_in "Number of tickets", with: 100
     click_button "Create"
 
@@ -22,6 +23,6 @@ feature "User creates event" do
     expect(page).to have_content(event_name)
     expect(page).to have_content("This is my first event")
     expect(page).to have_content("Dubai Mall")
-    expect(page).to have_content("7 Jan 2025 01:00 pm")
+    expect(page).to have_content(event_date.strftime("%-d %b %Y %I:%M %P"))
   end
 end
